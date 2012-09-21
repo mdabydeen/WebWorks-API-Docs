4db1526d81707efd5b7a13dce3b881e7e1076161
efeener <efeener@rim.com>
2012-09-21 14:17:00 -0400

    Updating user.identity API doc

--- api/blackberry_user_identity.js
+++ api/blackberry_user_identity.js
@@ -17,7 +17,6 @@
 /**
 * @toc {User Identity} User Identity 
 * @BB10X
-* @RIPPLE
 * @namespace The User Identity object contains information regarding the user&apos;s identity and accounts on a BlackBerry smartphone.
 *
 * <h3>Identity Service SDK Overview - interfaces for using Identity
@@ -136,7 +135,7 @@ blackberry.user.identity = {
  * The getVersion() function retrieves the version of the IDS library
  * that your application is using.
  * @BB10X
- * @returns {The version of IDS library}
+ * @returns {int}
  * 	"1000000"
  * @example 
 * &lt;script type=&quot;text&sol;javascript&quot;&gt;
@@ -153,28 +152,110 @@ getVersion : function(){},
  * at this time).
  * @returns {int} result
  * @BB10X
- * @RIPPLE
+ * @example 
+ * &lt;script type=&quot;text&sol;javascript&quot;&gt;
+	var registerResult = blackberry.user.identity.registerProvider("ids:rim:bbid");
+	if( registerResult.errorDescription ) {
+		alert("IDS register provider error: " + registerResult.result + " with description: " + registerResult.errorDescription);
+	}
+ * &lt;&sol;script&gt;
 */
 registerProvider : function(name) { },
 
 
 /**
- * Applications use this function to register which Identity Provider(s) that
- * they want to use.  Once the application has initialized the library, it can
- * register for each of the identity providers it is interested in.
-* @returns {ids_result_t}
-* @BB10X
-* @RIPPLE
-*/
+ * Configure an option in the Identity Services library.  These options allow an
+ * application to modify the default behavior of the IDS library as well as
+ * configure the way the library and application integrate together.
+ * <p>Available options are:
+ * <p> Set GUI Allowed (option = 0, value = true/false)
+ * <p>Option to set whether or not GUI allowed flag is set.  Value is "true" or
+ * "false".  By default, in cases where user input is required, the Identity
+ * Service performs the user interaction on behalf of the calling
+ * application.  If this has been set to false, and user input is required,
+ * the API will return an error.  If no user interaction is required to
+ * complete the API, this setting has no effect.
+ * <p> Set Group ID (option = 1, value = groupId)
+ * <p> Set the group ID of the calling application for UI dialogs.  String
+ * version of UI group id.  This is required in cases where the Identity
+ * Service requires user input from the user and must open an interface in
+ * the context of the calling application.
+ * <p> Set Verbosity (option = 2, value = {"Silent", "Normal", "Verbose")
+ * <p> Set the library logging verbosity level.  Useful during application
+ * development to allow developers to be able to increase logging for
+ * diagnostics. Note that IDS logs are generated on stderr, and so should be
+ * captured in the application's log file.
+ * @param {int} option The option to be modified.
+ * @param {String} value The new string value to set
+ * @returns {int}
+ * @BB10X
+ * @example
+ * &lt;script type=&quot;text&sol;javascript&quot;&gt;
+	var setOptionResult = blackberry.user.identity.setOption(0, true);
+	if( setOptionResult.errorDescription ) {
+		alert("IDS set option error: " + setOptionResult.result + " with description: " + setOptionResult.errorDescription);
+	}
+ * &lt;&sol;script&gt;
+ */
 setOption : function(option, value) { },
 
 /**
- * Applications use this function to register which Identity Provider(s) that
- * they want to use.  Once the application has initialized the library, it can
- * register for each of the identity providers it is interested in.
-* @returns {ids_result_t}
-* @BB10X
-* @RIPPLE
+ * Issue a get token request.
+ *
+ * @param {String} provider The identity provider to send this request to.
+ * @param {String} tokenType A token type. It must be NOT NULL
+ * and 32 characters >= tokenType > 0 character
+ * @param {String} appliesTo The application or service to which the token applies. It
+ * must be NOT NULL and 96 characters >= applies_to > 0 character
+ * @callback {Function} successCallback Function which is invoked upon successful operation of this
+ * method.
+ * @callback {JSON} successCallback.tokenResult A JSON object containing a token and it's information in the form below:
+     * <pre>{
+     * "token": "ghdaj23i8rghaepeghk;a",
+	 * "params": ["name":"TOKEN_SECRET","value":"oguireqhge"]
+     * }</pre>
+ * @callback {Function} failureCallback Function which is invoked when this method fails. This
+ * callback contains an errorCode parameter to specify the failure condition.
+ * @callback {JSON} failureCallback.result A JSON object containing details of the failure in the form below:
+	*<pre>{
+	* "result":"50009",
+	* "errorDescription":"The appliesTo value is not valid"
+	* }</pre>
+ *
+ * <p><b>Error Handling:</b><br>
+ * Requests that do not complete successfully will result in the failure
+ * callback being called with one of the following result codes:
+ * <p>- 49999: An internal error has occurred attempting to process
+ * the request.
+ * <p>- 50009: The appliesTo value is not valid.
+ * <p>- 50008: The tokenType value is not valid.
+ * <p>- 50010: There are not enough resources available to
+ * process the request.
+ * <p>- 50003: The account is currently locked, token
+ * access is unavailable while locked.
+ * <p>- 50004: The user could not be authenticated.
+ * <p>- 50011: The service is currently offline and
+ * and cannot retreive the requested token.
+ * <p>- 50012: An error occurred communicating with
+ * the service.
+ * @returns {void}
+ * @BB10X
+ * @example
+ * &lt;script type=&quot;text&sol;javascript&quot;&gt;
+	function getTokenSuccess(tokenResult) {
+		alert("Received token: " + tokenResult.token);
+		for( param in tokenResult.tokenParams ) {
+			alert("Token param: " + tokenResult.tokenParams[param].name + " and value: " + tokenResult.tokenParams[param].value);
+		}
+	}
+			
+	function getTokenFailure(result) {
+		alert("Failed to retrieve token - result is: " + result.result + " with description: " + result.errorDescription);
+	}
+
+	blackberry.user.identity.registerProvider("ids:rim:bbid");
+	blackberry.user.identity.getToken("ids:rim:bbid", "MyTokenType", "urn:token:myapplication", getTokenSuccess, getTokenFailure);
+ * &lt;&sol;script&gt;
 */
 getToken : function(idsProvider, tokenType, appliesTo, successCallback, failureCallback) { },
 
@@ -191,38 +272,103 @@ getToken : function(idsProvider, tokenType, appliesTo, successCallback, failureC
  * must be NOT NULL and 96 characters >= applies_to > 0 character
  * @callback {Function} successCallback Function which is invoked upon successful operation of this
  * method.
+ * @callback {JSON} successCallback.result A JSON object containing the in the form below:
+     * <pre>{
+     * "result": true
+     * }</pre>
  * @callback {Function} failureCallback Function which is invoked when this method fails. This
  * callback contains an errorCode parameter to specify the failure condition.
+ * @callback {JSON} failureCallback.result A JSON object containing details of the failure in the form below:
+	*<pre>{
+	* "result":"50009",
+	* "errorDescription":"The appliesTo value is not valid"
+	* }</pre>
  *
- * <b>Error Handling:</b><br>
+ * <p><b>Error Handling:</b><br>
  * Requests that do not complete successfully will result in the failure
  * callback being called with one of the following result codes:
- * - 49999: An internal error has occurred attempting to process
+ * <p>- 49999: An internal error has occurred attempting to process
  * the request.
- * - 50009: The appliesTo value is not valid.
- * - 50008: The tokenType value is not valid.
- * - 50010: There are not enough resources available to
+ * <p>- 50009: The appliesTo value is not valid.
+ * <p>- 50008: The tokenType value is not valid.
+ * <p>- 50010: There are not enough resources available to
  * process the request.
- * - 50003: The account is currently locked, token
+ * <p>- 50003: The account is currently locked, token
  * access is unavailable while locked.
- * - 50004: The user could not be
+ * <p>- 50004: The user could not be
  * authenticated.
+ * @returns {void}
+ * @BB10X
+ * @example
+ * &lt;script type=&quot;text&sol;javascript&quot;&gt;
+	function clearTokenSuccess(clearTokenResult) {
+		alert("Token cleared: " + clearTokenResult.cleared);
+	}
+			
+	function clearTokenFailure(result) {
+		alert("Failed to clear token - result is: " + result.result + " with description: " + result.errorDescription);
+	}
 
- 
- 
- * @returns {ids_result_t}
-* @BB10X
-* @RIPPLE
+	blackberry.user.identity.registerProvider("ids:rim:bbid");
+	blackberry.user.identity.clearToken("ids:rim:bbid", "MyTokenType", "urn:token:myapplication", clearTokenSuccess, clearTokenFailure);
+ * &lt;&sol;script&gt;
 */
 clearToken : function(idsProvider, tokenType, appliesTo, successCallback, failureCallback) { },
 
 /**
- * Applications use this function to register which Identity Provider(s) that
- * they want to use.  Once the application has initialized the library, it can
- * register for each of the identity providers it is interested in.
-* @returns {ids_result_t}
-* @BB10X
-* @RIPPLE
+ * Issue a request for a property.
+ *
+ * @param {String} provider The identity provider to send this request to.
+ * @param {String} propertyList A comma separated string listing each of the properties
+ * requested
+ * @callback {Function} successCallback Function which is invoked upon successful operation of this
+ * method.
+ * @callback {JSON} successCallback.result A JSON object containing the in the form below:
+     * <pre>{[
+ * 		{"uri":"urn:bbid:firstname":"value":"john"},
+		{"uri":"urn:bbid:lastname":"value":"doe"}
+     * }</pre>
+ * @callback {Function} failureCallback Function which is invoked when this method fails. This
+ * callback contains an errorCode parameter to specify the failure condition.
+ * @callback {JSON} failureCallback.result A JSON object containing details of the failure in the form below:
+	*<pre>{
+	* "result":"50004",
+	* "errorDescription":"The user could not be authenticated."
+	* }</pre>
+ * <p><b>Error Handling:</b><br>
+ * Requests that do not complete successfully will result in the failure
+ * callback being called with one of the following result codes:
+ * <p>- 49999: An internal error has occurred attempting to process
+ * the request.
+ * <p>- 50010: There are not enough resources available to
+ * process the request.
+ * <p>- 50003: The account is currently locked, token
+ * access is unavailable while locked.
+ * <p>- 50004: The user could not be authenticated.
+ * <p>- 50011: The service is currently offline and
+ * and cannot retreive the requested token.
+ * <p>- 50002: The length of a property name in the list exceeds
+ * the maximum name length (32).
+ * <p>- 50017: The application does not have access to
+ * one of the requested properties.
+ * @returns {void}
+ * @BB10X
+ * @example
+ * &lt;script type=&quot;text&sol;javascript&quot;&gt;
+	function getPropertiesSuccess(properties) {
+		alert("Get properties returned the following user properties:");
+		for( prop in properties ) {
+			alert("Property: " + properties[prop].uri + " value: " + properties[prop].value);
+		}
+	}
+
+	function getPropertiesFailure(result) {
+		alert("Failed to retrieve user properties: " + result.result + " details: " + result.failureInfo);
+	}
+
+	blackberry.user.identity.registerProvider("ids:rim:bbid");
+	blackberry.user.identity.getProperties("ids:rim:bbid", "urn:bbid:firstname,urn:bbid:lastname", getPropertiesSuccess, getPropertiesFailure);
+ * &lt;&sol;script&gt;
 */
 getProperties : function(idsProvider, userProperties, successCallback, failureCallback) { };
 
